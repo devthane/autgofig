eb86840633f742b4fc1a5313b581545a36b409ac
kaelen baldwin <kabaldwin@averittexpress.com>
2018-01-30 13:24:50 -0600

    NewLoader and LoadConfig now both require an configOut parameter which will have it's exported fields set to the fields loaded from the config file specified by "." + projectName + ".yml"

--- test/gofig/integration_test.go
+++ test/gofig/integration_test.go
@@ -4,77 +4,54 @@ import (
 	"github.com/thane421/gofig/pkg/gofig"
 	"testing"
 	"os"
+	"runtime"
+	"fmt"
 )
+
+type TestObj struct {
+	StringField string
+	IntField    int
+}
 
 func TestIntegrationYaml(t *testing.T) {
 	var (
-		var1  string
-		var2  string
-		var3  string
-		var4  string
-		var5  string
-		var6  string
-		var7  string
-		var8  string
-		var9  string
-		var10 string
+		stringField  string
+		intField     int
+		Separator    string
+		HomeDir      string
+		ConfLocation string
 	)
 
-	l := gofig.NewLoader([]string{
-		"var1",
-		"var2",
-		"var3",
-		"var4",
-		"var5",
-		"var6",
-		"var7",
-		"var8",
-		"var9",
-		"var10",
-	}, "config-test")
+	name := ".config-test"
 
-	config := *l.Config
-	defer os.Remove(l.ConfLocation)
+	if runtime.GOOS == "windows" {
+		Separator = "\\"
+		HomeDir = os.Getenv("userprofile")
+		ConfLocation = HomeDir + Separator + name + ".yml"
+	} else {
+		Separator = "/"
+		HomeDir = os.Getenv("HOME")
+		ConfLocation = HomeDir + Separator + name + ".yml"
+	}
 
-	var1 = config["var1"]
-	var2 = config["var2"]
-	var3 = config["var3"]
-	var4 = config["var4"]
-	var5 = config["var5"]
-	var6 = config["var6"]
-	var7 = config["var7"]
-	var8 = config["var8"]
-	var9 = config["var9"]
-	var10 = config["var10"]
+	file, err := os.Create(ConfLocation)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	if var1 == "" {
-		t.Fail()
-	}
-	if var2 == "" {
-		t.Fail()
-	}
-	if var3 == "" {
-		t.Fail()
-	}
-	if var4 == "" {
-		t.Fail()
-	}
-	if var5 == "" {
-		t.Fail()
-	}
-	if var6 == "" {
-		t.Fail()
-	}
-	if var7 == "" {
-		t.Fail()
-	}
-	if var8 == "" {
-		t.Fail()
-	}
-	if var9 == "" {
-		t.Fail()
-	}
-	if var10 == "" {
+	file.Write([]byte(
+		fmt.Sprintf("StringField: test\nIntField: \"5\"\n"),
+	))
+	file.Close()
+	defer os.Remove(ConfLocation)
+
+	config := new(TestObj)
+	gofig.LoadConfig(config, name)
+
+	stringField = config.StringField
+	intField = config.IntField
+
+	if stringField != "test" || intField != 5 {
 		t.Fail()
 	}
 
